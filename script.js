ae53bfd8eae7bd4d03dd6033736acb86ace956f7
ismaile <ismailearliene@gmail.com>
2022-12-10 15:06:22 -0300

    Cunsumundo API VIACEP

---
+++ script.js
@@ -0,0 +1,59 @@
+let cep = document.getElementById('cep');
+let logradouro = document.querySelector('#logradouro');
+let complemento = document.querySelector('#complemento');
+let bairro = document.querySelector('#bairro');
+let localidade = document.querySelector('#localidade');
+let uf = document.querySelector('#uf');
+let btn = document.querySelector('#btn');
+let reset = document.querySelector('#reset');
+let inviseble = document.querySelector('.invisible')
+let loader = document.querySelector('.loader')
+
+btn.addEventListener('click',()=>{
+    let url = `http://viacep.com.br/ws/${cep.value}/json/`;
+   
+fetch(url).then(resposta=>{
+    return resposta.json();
+}).then(data=>{
+    logradouro.value = data.logradouro;
+    bairro.value = data.bairro;
+    localidade.value = data.localidade;
+    uf.value = data.uf;
+}).catch(err=>{
+    alert(err)
+    logradouro.value = ""
+    bairro.value = ""
+    localidade.value = ""
+    uf.value = "";
+    inviseble.style.display="none"
+  
+});
+
+if(cep.value){
+    
+    loader.style.display="flex"
+}
+
+function tempo(){
+    inviseble.style.display="block"
+
+   loader.style.display="none"
+  
+   
+
+}
+setTimeout( tempo, 2000);
+});
+
+
+cep.addEventListener('keypress',(e)=>{
+    if(e.keyCode ==13){
+        btn.click()
+    }
+})
+
+reset.addEventListener('click',()=>{
+    if(reset){
+        inviseble.style.display="none"
+    }
+})
